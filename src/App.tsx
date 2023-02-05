6c8aed3e74c43348158b7c37e59c0ad2e939d756
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-05 14:28:48 +0300

    feat: add two pages, implement code-splitting + via lazy and Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export const App = () => {
 	return (
 		<div className="app">
-			<Counter />
+			<Link to="/">Main page</Link>
+			<Link to="/about">About page</Link>
+
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path="/" element={<MainPageAsync />} />
+					<Route path="/about" element={<AboutPageAsync />} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import { Counter } from "../../components/Counter";
+
+const MainPage = () => {
+	return (
+		<div>
+			<h1>Main Page</h1>
+
+			<Counter />
+		</div>
+	);
+};
+
+export default MainPage;

600590412cb1f624015e741bc1d2924e32adb1d8
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-05 15:18:01 +0300

    feat: theme context logic, useTheme hook + ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import cn from 'classnames';
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
 	return (
-		<div className="app">
+		<div className={cn('app', theme)}>
+			<div>
+				<button onClick={toggleTheme}>Toggle theme</button>
+			</div>
+
 			<Link to="/">Main page</Link>
 			<Link to="/about">About page</Link>
 

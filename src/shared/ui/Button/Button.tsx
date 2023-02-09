599ac28105584d6cf698505ae689bb844da8e851
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-09 18:32:34 +0300

    feat: Button component + clear theme

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,25 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import { classNames } from 'shared/lib';
+import styles from './Button.module.scss';
+
+export enum ButtonTheme {
+	CLEAR = 'clear'
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
+	theme?: ButtonTheme;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+	const { children, className, theme = ButtonTheme.CLEAR, ...rest } = props;
+
+	return (
+		<button
+			{...rest}
+			className={classNames(styles.Button, {}, [styles[theme]])}
+		>
+			{children}
+		</button>
+	);
+};

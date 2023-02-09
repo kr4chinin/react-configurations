9aaa84aae726cb78f56cdab88d2b225bcd2b0d8d
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-09 18:29:26 +0300

    fix: updated modules declarations, add new + for .png, .jpg, .jpeg, .gif, .svg

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,1 +1,17 @@
-declare module '*.module.scss';
+declare module '*.scss' {
+	interface IClassNames {
+		[className: string]: string;
+	}
+	const classNames: IClassNames;
+	export = classNames;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg' {
+	import React from 'react';
+	const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+	export default SVG;
+}

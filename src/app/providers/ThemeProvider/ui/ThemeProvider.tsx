c957da2017b2dad8ffd394d88bc79795a0d3dd3e
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-09 18:31:48 +0300

    fix: default -> named export in ThemeProvider

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContex
 const defaultTheme =
 	(localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
+export const ThemeProvider: FC = ({ children }) => {
 	const [theme, setTheme] = useState<Theme>(defaultTheme);
 
 	const defaultProps = useMemo(
@@ -21,5 +21,3 @@ const ThemeProvider: FC = ({ children }) => {
 		</ThemeContext.Provider>
 	);
 };
-
-export default ThemeProvider;

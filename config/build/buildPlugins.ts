ef741e48569d5582c58627ca00b6cb3501283db8
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-11 22:33:45 +0300

    feat: define global env var __IS_DEV__

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,9 +1,12 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { ProgressPlugin, WebpackPluginInstance } from 'webpack';
-import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
+export function buildPlugins({
+	paths,
+	isDev
+}: BuildOptions): WebpackPluginInstance[] {
 	return [
 		new HtmlWebpackPlugin({
 			template: paths.html
@@ -12,6 +15,9 @@ export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
 		new MiniCssExtractPlugin({
 			filename: 'css/[name].[contenthash:8].css',
 			chunkFilename: 'css/[name].[contenthash:8].chunk.css'
+		}),
+		new DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev)
 		})
 	];
 }

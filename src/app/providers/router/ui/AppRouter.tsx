6c9aa71bb230b7ea650a6817fdfa5836a59349d2
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-13 22:44:09 +0300

    fix: add missing translations to AppRouter + for 'Loading...'

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,13 @@
 import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 export const AppRouter = () => {
+	const { t } = useTranslation();
+
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
+		<Suspense fallback={<div>{t('Loading')}...</div>}>
 			<Routes>
 				{Object.values(routeConfig).map(route => (
 					<Route key={route.path} {...route} />

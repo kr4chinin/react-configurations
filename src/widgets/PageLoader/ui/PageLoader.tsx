2eeb1b82e3c5d10c38685be92660188996fec208
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-14 23:19:16 +0300

    feat: PageLoader widget

---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,17 @@
+import { classNames } from 'shared/lib';
+import { Loader } from 'shared/ui/Loader/Loader';
+import styles from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+	className?: string;
+}
+
+export const PageLoader = (props: PageLoaderProps) => {
+	const { className } = props;
+
+	return (
+		<div className={classNames(styles.PageLoader, {}, [className])}>
+			<Loader />
+		</div>
+	);
+};

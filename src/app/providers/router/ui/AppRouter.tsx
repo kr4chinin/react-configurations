fef4dd80ddc02fafb5b8233b2adbf4b1a0b9f247
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-14 23:19:41 +0300

    feat: + PageLoader in AppRouter glob Suspense

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,13 +1,11 @@
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 export const AppRouter = () => {
-	const { t } = useTranslation();
-
 	return (
-		<Suspense fallback={<div>{t('Loading')}...</div>}>
+		<Suspense fallback={<PageLoader />}>
 			<Routes>
 				{Object.values(routeConfig).map(route => (
 					<Route key={route.path} {...route} />

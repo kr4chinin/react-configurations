309e03cb774fa73117a9ca3741a52c117da7332c
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-15 21:56:44 +0300

    feat: + PageError widget

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib';
+import { Button } from 'shared/ui/Button';
+import styles from './PageError.module.scss';
+
+interface PageErrorProps {
+	className?: string;
+}
+
+export const PageError = (props: PageErrorProps) => {
+	const { className } = props;
+
+	const { t } = useTranslation();
+
+	const reloadPage = () => {
+		window.location.reload();
+	};
+
+	return (
+		<div className={classNames(styles.PageError, {}, [className])}>
+			<h1 className={styles.title}>{t('Something went wrong')}</h1>
+
+			<Button onClick={reloadPage}>{t('Reload page')}</Button>
+		</div>
+	);
+};

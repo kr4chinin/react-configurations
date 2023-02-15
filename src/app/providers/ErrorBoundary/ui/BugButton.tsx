2093c69e5dc94dcb9f81d0e5d2bcececd13fec99
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-15 21:57:27 +0300

    feat: + BugButton for testing ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button';
+
+export const BugButton = () => {
+	const [isError, setIsError] = useState(false);
+
+	const throwError = () => {
+		setIsError(true);
+	};
+
+	useEffect(() => {
+		if (isError) {
+			throw new Error('Test error from BugButton');
+		}
+	}, [isError]);
+
+	// eslint-disable-next-line i18next/no-literal-string
+	return <Button onClick={throwError}>✗</Button>;
+};

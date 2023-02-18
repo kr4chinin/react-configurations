3ee2e41aadefb28d92a78a3df8c1a1c645b9af40
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-18 22:16:09 +0300

    test: refactor imports in Sidebar.test.tsx

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,5 +1,4 @@
-import { screen, fireEvent } from '@testing-library/react';
-import { withTranslation } from 'react-i18next';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from './Sidebar';
 

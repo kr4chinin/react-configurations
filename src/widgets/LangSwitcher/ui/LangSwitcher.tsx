222c0a99c377dda30ea08da6fae32c17fd320349
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-18 21:59:45 +0300

    fix: toggle lang logic, imports

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from 'react-i18next';
-import i18n from 'shared/config/i18n/i18n';
 import { classNames } from 'shared/lib';
 import { Button } from 'shared/ui/Button';
 
@@ -10,13 +9,14 @@ interface LangSwitcherProps {
 export const LangSwitcher = (props: LangSwitcherProps) => {
 	const { className } = props;
 
-	const { t } = useTranslation();
+	const { t, i18n } = useTranslation();
+
+	const toggle = () => {
+		i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+	};
 
 	return (
-		<Button
-			className={classNames('', {}, [className])}
-			onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en')}
-		>
+		<Button className={classNames('', {}, [className])} onClick={toggle}>
 			{t('Language')}
 		</Button>
 	);

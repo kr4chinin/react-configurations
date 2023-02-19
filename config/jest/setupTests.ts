0f2a4405a7443ff60d49e9d32557c528fc5cd4f8
Ilya Kruchinin <kr4chinin@mail.ru>
2023-02-19 23:45:58 +0300

    chore: mock react-i18next useTranslation hook

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -1,2 +1,10 @@
 import '@testing-library/jest-dom';
-import 'regenerator-runtime/runtime';
+
+jest.mock('react-i18next', () => ({
+	useTranslation: () => ({
+		t: (str: string) => str,
+		i18n: {
+			changeLanguage: () => new Promise(() => {}),
+		},
+	}),
+}));

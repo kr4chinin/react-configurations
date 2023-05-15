8ba0ad35da7b75984b6f01a214e39f9dbbb761d8
Ilya Kruchinin <kr4chinin@mail.ru>
2023-05-15 22:29:16 +0300

    chore: add __IS_DEV__ global var to globals + Fix issues with entities/* imports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,6 +1,9 @@
 import path from 'path';
 
 export const config = {
+	globals: {
+		__IS_DEV__: true,
+	},
 	testEnvironment: 'jsdom',
 	clearMocks: true,
 	coveragePathIgnorePatterns: ['/node_modules/'],
@@ -22,6 +25,7 @@ export const config = {
 	moduleNameMapper: {
 		'\\.scss$': 'identity-obj-proxy',
 		'\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+		'entities/(.*)': '<rootDir>src/entities/$1',
 	},
 };
 

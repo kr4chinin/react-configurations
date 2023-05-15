ac6fc52981463921f4ca78549e11db09373ac450
Ilya Kruchinin <kr4chinin@mail.ru>
2023-05-15 22:30:15 +0300

    feat: store provider, state schema

---
+++ src/entities/Counter/index.tsx
@@ -0,0 +1,5 @@
+import { counterReducer } from './model/slice/counterSlice';
+import { CounterSchema } from './model/types/counterSchema';
+import { Counter } from './ui/Counter';
+
+export { Counter, CounterSchema, counterReducer };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { App } from 'app/App';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import 'app/styles/index.scss';
 import { render } from 'react-dom';
@@ -7,12 +8,15 @@ import { BrowserRouter } from 'react-router-dom';
 import 'shared/config/i18n/i18n';
 
 render(
-	<BrowserRouter>
-		<ErrorBoundary>
-			<ThemeProvider>
-				<App />
-			</ThemeProvider>
-		</ErrorBoundary>
-	</BrowserRouter>,
+	<StoreProvider>
+		<BrowserRouter>
+			<ErrorBoundary>
+				<ThemeProvider>
+					<App />
+				</ThemeProvider>
+			</ErrorBoundary>
+		</BrowserRouter>
+	</StoreProvider>,
+
 	document.getElementById('root'),
 );

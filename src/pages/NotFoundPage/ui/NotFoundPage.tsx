280afa32707fbaa9c6b67c615249f9efff2de058
Ilya Kruchinin <kr4chinin@mail.ru>
2023-05-21 22:29:38 +0300

    fix: remove excess props from NotFoundPage

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,19 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { classNames } from 'shared/lib';
 import styles from './NotFoundPage.module.scss';
 
-interface NotFoundPageProps {
-	className?: string;
-}
-
-export const NotFoundPage = (props: NotFoundPageProps) => {
-	const { className } = props;
-
+export const NotFoundPage = () => {
 	const { t } = useTranslation();
 
-	return (
-		<div className={classNames(styles.NotFoundPage, {}, [className])}>
-			{t('page_not_found')}
-		</div>
-	);
+	return <div className={styles.NotFoundPage}>{t('page_not_found')}</div>;
 };

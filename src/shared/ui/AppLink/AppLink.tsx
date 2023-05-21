bc8ff32d359bd2b042ed8cc43346855688748d9c
Ilya Kruchinin <kr4chinin@mail.ru>
2023-05-21 22:08:11 +0300

    style: refactor AppLink and Button styles, v2

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -3,24 +3,15 @@ import { Link, LinkProps } from 'react-router-dom';
 import { classNames } from 'shared/lib';
 import styles from './AppLink.module.scss';
 
-export enum AppLinkTheme {
-	PRIMARY = 'primary',
-	SECONDARY = 'secondary',
-}
-
 interface AppLinkProps extends LinkProps {
 	className?: string;
-	theme?: AppLinkTheme;
 }
 
 export const AppLink: FC<AppLinkProps> = props => {
-	const { className, children, theme = AppLinkTheme.PRIMARY, ...rest } = props;
+	const { className, children, ...rest } = props;
 
 	return (
-		<Link
-			{...rest}
-			className={classNames(styles.AppLink, {}, [className, styles[theme]])}
-		>
+		<Link {...rest} className={classNames(styles.AppLink, {}, [className])}>
 			{children}
 		</Link>
 	);
